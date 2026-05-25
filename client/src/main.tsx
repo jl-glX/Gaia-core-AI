ca4710e7a75cde22c1ffbf2ce8ba12cb5d20368b
Javier López Díaz <jld890045@tutamail.com>
2026-05-25 16:48:08 +0200

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,20 +2,28 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './index.css';
+import "./index.css";
 
 const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-function updateDarkClass(e = null) {
-  const isDark = e ? e.matches : darkQuery.matches;
+function updateDarkClass(event?: MediaQueryListEvent) {
+  const isDark = event ? event.matches : darkQuery.matches;
   document.documentElement.classList.toggle('dark', isDark);
 }
 
 updateDarkClass();
 darkQuery.addEventListener('change', updateDarkClass);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+darkQuery.addEventListener("change", updateDarkClass);
